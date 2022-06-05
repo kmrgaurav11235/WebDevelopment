8aca3317cde7ce7a61245e11e1dddb088222010f
Kumar Gaurav <kmrgaurav11235@gmail.com>
2022-06-05 11:50:54 +0530

    Solution to DOM challenge

--- 05-javascript/02-DOMChallenge/index.js
+++ 05-javascript/02-DOMChallenge/index.js
@@ -1,1 +1,3 @@
-document.query
+// var thirdItem = document.firstElementChild.lastElementChild.lastElementChild.lastElementChild; // Doesn't works becuase we have to add <script> tag
+var thirdItem = document.querySelector("ul").lastElementChild;
+thirdItem.innerHTML = "Gaurav";

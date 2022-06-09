aa6471f5bb877ea439f5c792b09f5dd0fe78b408
Kumar Gaurav <kmrgaurav11235@gmail.com>
2022-06-09 09:21:48 +0530

    Dicee Challenge Step 4 - Change the <img> to a Random Dice

--- 05-javascript/03-DiceeChallenge/index.js
+++ 05-javascript/03-DiceeChallenge/index.js
@@ -1,2 +1,4 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-console.log(randomNumber1);
+var imageSrc1 = "images/dice" + randomNumber1 + ".png";
+
+document.querySelector(".img1").setAttribute("src", imageSrc1);

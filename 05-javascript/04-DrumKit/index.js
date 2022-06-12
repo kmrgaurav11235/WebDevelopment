92e30acca3c360bd9c82d217d300fb540b1cd767
Kumar Gaurav <kmrgaurav11235@gmail.com>
2022-06-12 13:18:34 +0530

    Anonymous function

--- 05-javascript/04-DrumKit/index.js
+++ 05-javascript/04-DrumKit/index.js
@@ -1,12 +1,10 @@
-function playAudio() {
-  var audio = new Audio("sounds/tom-1.mp3");
-  audio.play();
-}
-
 // document.querySelector("button").addEventListener("click", handleClick);
 
 var buttons = document.querySelectorAll(".drum");
 
 for (var i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", playAudio);
+  buttons[i].addEventListener("click", function () {
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+  });
 }

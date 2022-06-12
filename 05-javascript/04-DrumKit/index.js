0c0d251ce80213ef07c379a129a528eadb358228
Kumar Gaurav <kmrgaurav11235@gmail.com>
2022-06-12 13:26:59 +0530

    Using 'this' keyword to get access to element that triggered the event listener

--- 05-javascript/04-DrumKit/index.js
+++ 05-javascript/04-DrumKit/index.js
@@ -4,6 +4,9 @@ var buttons = document.querySelectorAll(".drum");
 
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
+    console.log(this.innerHTML);
+    this.style.color = "white";
+
     var audio = new Audio("sounds/tom-1.mp3");
     audio.play();
   });

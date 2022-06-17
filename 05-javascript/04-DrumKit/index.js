e181660115eb66e8e78de3c9c8dad8e6d04155a7
Kumar Gaurav <kmrgaurav11235@gmail.com>
2022-06-17 20:09:53 +0530

    Using Keyboard Event Listeners to play sound

--- 05-javascript/04-DrumKit/index.js
+++ 05-javascript/04-DrumKit/index.js
@@ -1,75 +1,56 @@
+function playAudio(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-1.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default:
+      console.log(key);
+  }
+}
+
+// Detecting button press
 // document.querySelector("button").addEventListener("click", handleClick);
-
 var buttons = document.querySelectorAll(".drum");
-
 for (var i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", function () {
+  buttons[i].addEventListener("click", function() {
     var buttonInnerHtml = this.innerHTML;
-    switch (buttonInnerHtml) {
-      case "w":
-        var tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-
-      case "a":
-        var tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-
-      case "a":
-        var tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-
-      case "s":
-        var tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-
-      case "d":
-        var tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
-
-      case "j":
-        var snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-
-      case "k":
-        var crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-
-      case "l":
-        var kick = new Audio("sounds/kick-bass.mp3");
-        kick.play();
-        break;
-
-      default:
-        console.log(buttonInnerHtml)
-    }
-
-
+    playAudio(buttonInnerHtml);
   });
 }
 
-// Listening to Keyboard press
-document.addEventListener("keydown", function (event) {
-  console.log(event);
+// Detecting Keyboard press
+document.addEventListener("keydown", function(event) {
+  playAudio(event.key)
 });
-
-// Javascript objects and their methods
-function HouseKeeper(name, age, dob, workingFor) {
-    this.name = name;
-    this.age = age;
-    this.dob = dob;
-    this.workingFor = workingFor;
-    this.clean = function () {
-        console.log("Cleaning...");
-    }
-}
-
-var houseKeeper1 = new HouseKeeper("Raman", 42, "12-02-1984", 3.7);
-console.log(houseKeeper1.name);
-houseKeeper1.clean();
